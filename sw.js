353d43b7bc8c63b34a15d041b52e98bacb6cf026
widumir2026 <widuu@icloud.com>
2026-02-05 18:59:45 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="thai-cards-fixed";self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"])))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
